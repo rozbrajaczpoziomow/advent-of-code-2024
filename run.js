7be5982d5a393a6783a59ab04d6f4d48f689fc6d
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-01 16:54:26 +0100

    submitting

--- run.js
+++ run.js
@@ -1,12 +1,14 @@
 import { readFile, stat, writeFile } from 'node:fs/promises';
 
+const year = 2024;
+
 let aocSession;
 async function downloadInput(day) {
 	console.log(`> Downloading input for day ${day}`);
 	if(aocSession == null)
 		aocSession = await readFile('../aocSession.txt');
 
-	const resp = await fetch(`https://adventofcode.com/2024/day/${day}/input`, {
+	const resp = await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
 		headers: {
 			Cookie: `session=${aocSession}`
 		}
@@ -20,6 +22,53 @@ async function downloadInput(day) {
 		console.error(`Couldn't download input (response code ${resp.status}) - aoc session expired?`);
 }
 
+// thanks to myself: https://github.com/rozbrajaczpoziomow-kt/aoc-kotlin-template/blob/main/src/main/kotlin/AOCInteractions.kt#L40
+let state;
+async function submit(day, part, sol) {
+	if(aocSession == null)
+		aocSession = await readFile('../aocSession.txt');
+
+	if(state == null) {
+		try {
+			state = JSON.parse((await readFile('../state.json')).toString());
+		} catch {
+			state = { submitted: [] };
+		}
+	}
+
+	const key = `${day}.${part}`;
+	if(state.submitted.includes(key))
+		return;
+
+	process.stdout.write(`Submit "${sol}" to day ${day} pt ${part}? [yn] `);
+	let res;
+	const promise = new Promise(_res => { res = _res; });
+	process.stdin.once('data', buf => res(buf[0] === 'y'.charCodeAt(0)));
+	const doSubmit = await promise;
+	if(!doSubmit) {
+		console.log('n');
+		return;
+	}
+
+	console.log('y');
+	const resp = await fetch(`https://adventofcode.com/${year}/day/${day}/answer`, {
+		method: 'POST',
+		body: `level=${part}&answer=${encodeURIComponent(sol)}`,
+		headers: {
+			Cookie: `session=${aocSession}`,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	});
+	const text = (await resp.text()).split('<article><p>')[1].split('</p></article>')[0].replaceAll(/<\/?.*?>/g, '').replaceAll('  ', ' ').replace('If you\'re stuck, make sure you\'re using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. ', '');
+	console.log(text);
+
+	if(!text.includes('not the right answer') && !text.includes('gave an answer too recently')) {
+		state.submitted.push(key);
+		await writeFile('../state.json', JSON.stringify(state, null, '\t'));
+	}
+}
+
+
 const days = [];
 if(process.argv[2]) {
 	if(process.argv[2] === 'all')
@@ -28,7 +77,7 @@ if(process.argv[2]) {
 		days.push(...process.argv.slice(2).map(x => +x).filter(x => x && !Number.isNaN(x)));
 } else {
 	const date = new Date();
-	if(!process.cwd().includes(date.getFullYear()) || date.getMonth() !== 11) {
+	if(date.getFullYear() !== year || date.getMonth() !== 11) {
 		console.log('run.js [...days | all]');
 		process.exit(1);
 	}
@@ -47,15 +96,24 @@ for(const day of days) {
 		await downloadInput(day);
 	}
 	const { part1, part2 } = await import(`./${folder}/day.js`);
-	const startTime = performance.now();
+	let startTime = performance.now();
 	const ans1 = await part1();
-	if(ans1 !== null)
+	const part1Time = performance.now() - startTime;
+	if(ans1 !== null) {
 		console.log(`> Part 1:\n${ans1}\n`);
+		await submit(day, 1, ans1);
+	}
 
+	startTime = performance.now();
 	const ans2 = await part2();
-	if(ans2 !== null)
+	const part2Time = performance.now() - startTime;
+	if(ans2 !== null) {
 		console.log(`> Part 2:\n${ans2}`);
-	console.log(`\n${(performance.now() - startTime).toFixed(0)}ms`);
+		await submit(day, 2, ans2);
+	}
+	console.log(`\npt 1: ${part1Time.toFixed(1)}ms; pt 2: ${part2Time.toFixed(1)}ms`);
 
 	process.chdir(origCwd);
 }
+
+process.exit(0);
