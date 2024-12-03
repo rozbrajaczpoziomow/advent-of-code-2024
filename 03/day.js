00308bf5207278831c9c4d1903b7c2ed2ae0a14e
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-03 06:22:28 +0100

    day 3 horrorcode

--- 03/day.js
+++ 03/day.js
@@ -10,17 +10,39 @@ export async function part1() {
 	let ns = [], n = 0;
 
 	for(const line of input) {
-
+		for(const m of [...line.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)]) {
+			const a = +m[1]
+			const b = +m[2];
+			n += a*b;
+		}
 	}
-	return null;
+	return n;
 }
 
 export async function part2() {
 	let input = (await readFile('input.txt')).toString().split('\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
+	let on = true;
 
+	for(const line of input) {
+		for(const m of [...line.matchAll(/(mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\)))/g)]) {
+			const inst = m[1];
+			console.log(inst);
+			if(inst === 'don\'t()') {
+				on = false;
+				continue;
+			} else if(inst === 'do()') {
+				on = true;
+				continue
+			}
+			if(!on)
+				continue;
+			const a = +m[2]
+			const b = +m[3];
+			n += a*b;
+		}
 	}
-	return null;
+	return n;
+
 }
