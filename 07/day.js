ac34fc5a78be111caae24a881b6249b48240cda7
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-07 07:58:28 +0100

    day 7 - splitting the input into 5 pieces so the code would finish faster

--- 07/day.js
+++ 07/day.js
@@ -6,21 +6,53 @@ const count = (arr, what) => arr.filter(el => el === what).length;
 const { abs, sqrt, sin, cos, floor, ceil, round } = Math;
 
 export async function part1() {
-	let input = (await readFile('input.txt')).toString().split('\n');
+	// let input = (await readFile('input.txt')).toString().split('\n');
+	let input = [];
 	let ns = [], n = 0;
 
 	for(const line of input) {
-
+		let [wa, tmp] = line.split(': ');
+		wa = +wa;
+		ns = tmp.split(' ').map(x => +x);
+		let any = false;
+		let poss = [{w: wa, c: ns[0], ns:ns.slice(1)}];
+		while(poss.length > 0) {
+			const {w, c, ns} = poss.shift();
+			if(ns.length == 0) {
+				if(c==w){any=true;break;}
+				continue;
+			}
+			poss.push({w, c: c + ns[0], ns:ns.slice(1)});
+			poss.push({w, c: c * ns[0], ns:ns.slice(1)});
+		}
+		if(any)
+			n+=wa;
 	}
-	return null;
+	return n;
 }
 
 export async function part2() {
-	let input = (await readFile('input.txt')).toString().split('\n');
+	let input = (await readFile(process.env.FILE ?? 'input.txt')).toString().split('\n');
 	let ns = [], n = 0;
 
 	for(const line of input) {
-
+		let [wa, tmp] = line.split(': ');
+		wa = +wa;
+		ns = tmp.split(' ').map(x => +x);
+		let any = false;
+		let poss = [{w: wa, c: ns[0], ns:ns.slice(1)}];
+		while(poss.length > 0) {
+			const {w, c, ns} = poss.shift();
+			if(ns.length == 0) {
+				if(c==w){any=true;break;}
+				continue;
+			}
+			poss.push({w, c: c + ns[0], ns:ns.slice(1)});
+			poss.push({w, c: c * ns[0], ns:ns.slice(1)});
+			poss.push({w, c: +(`${c}${ns[0]}`), ns:ns.slice(1)});
+		}
+		if(any)
+			n+=wa;
 	}
-	return null;
+	return n;
 }
