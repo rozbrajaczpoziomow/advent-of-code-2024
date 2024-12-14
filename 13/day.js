0a1f05ef686d2734c5b59b6ce501b35bb8b99f0c
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-14 14:01:39 +0100

    day 13 & 14

--- 13/day.js
+++ 13/day.js
@@ -6,21 +6,38 @@ const count = (arr, what) => arr.filter(el => el === what).length;
 const { abs, sqrt, sin, cos, floor, ceil, round, min, max } = Math;
 
 export async function part1() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
+	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
+	for(let l of input) {
+		const [[ax, ay], [bx, by], [gx, gy]] = l.split('\n').map(x => x.slice(x.indexOf(': ')+2).split(', ').map(y => +y.slice(2)));
+		let sols = [];
+		for(let i = 0; i <= 100; ++i)
+			for(let j = 0; j <= 100; ++j)
+				if(gx - ax * i - bx * j == 0 && gy - ay * i - by * j == 0)
+					sols.push(3 * i + j);
 
+		if(sols.length)
+			n += min(...sols);
 	}
-	return null;
+
+	return n;
 }
 
 export async function part2() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
+	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
-
+	for(let l of input) {
+		let [[ax, ay], [bx, by], [gx, gy]] = l.split('\n').map(x => x.slice(x.indexOf(': ')+2).split(', ').map(y => +y.slice(2)));
+		gx += 10000000000000;
+		gy += 10000000000000;
+		let det = ax * by - ay * bx;
+		let a = floor((gx * by - gy * bx) / det);
+		let b = floor((ax * gy - ay * gx) / det);
+		if(ax * a + bx * b == gx && ay * a + by * b == gy)
+			n += a * 3 + b;
 	}
-	return null;
+
+	return n;
 }
--- 14/day.js
+++ 14/day.js
@@ -1,4 +1,4 @@
-import { readFile } from 'node:fs/promises';
+import { open, readFile } from 'node:fs/promises';
 
 const asc = (a, b) => a - b;
 const desc = (a, b) => b - a;
@@ -7,20 +7,99 @@ const { abs, sqrt, sin, cos, floor, ceil, round, min, max } = Math;
 
 export async function part1() {
 	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
-
-	for(const line of input) {
-
+	// let w = 11, h = 7;
+	let w = 101, h = 103;
+	let robots = []; // {x,y,vx,vy}[]
+	for(let line of input) {
+		const [[x, y], [vx, vy]] = line.split(' ').map(x => x.slice(2).split(',').map(y => +y));
+		robots.push({x,y,vx,vy});
 	}
-	return null;
+
+	for(let i = 0; i < 100; ++i) {
+		for(let robot of robots) {
+			let {x, y, vx, vy} = robot;
+			// console.log(x, y);
+			x += vx;
+			y += vy;
+			if(x < 0)
+				x += w;
+			if(y < 0)
+				y += h;
+			if(x >= w)
+				x -= w;
+			if(y >= h)
+				y -= h;
+			robot.x = x;
+			robot.y = y;
+		}
+	}
+
+	// let m = [];
+	// for(let x = 0; x < h; ++x) {
+	// 	m.push([]);
+	// 	for(let y = 0; y < w; ++y)
+	// 		m[x].push('.')
+	// }
+	let qrs = [0, 0, 0, 0];
+
+	const hx = floor(w / 2), hy = floor(h / 2);
+	for(let r of robots) {
+		let {x, y} = r;
+		if(x < hx && y < hy)
+			++qrs[0];
+		if(x > hx && y < hy)
+			++qrs[1];
+		if(x < hx && y > hy)
+			++qrs[2];
+		if(x > hx && y > hy)
+			++qrs[3];
+
+		// m[y][x] = m[y][x] == '.'? '1' : `${+m[y][x] + 1}`;
+	}
+
+	return qrs.reduce((a, b) => a*b);
+
+	// for(let l of m)
+	// 	console.log(l.join(''));
 }
 
 export async function part2() {
 	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
-
-	for(const line of input) {
-
+	// let w = 11, h = 7;
+	let w = 101, h = 103;
+	let robots = []; // {x,y,vx,vy}[]
+	for(let line of input) {
+		const [[x, y], [vx, vy]] = line.split(' ').map(x => x.slice(2).split(',').map(y => +y));
+		robots.push({x,y,vx,vy});
 	}
-	return null;
+
+	for(let i = 0; i < 100000; ++i) {
+		let m = [];
+		for(let x = 0; x < h; ++x) {
+			m.push([]);
+			for(let y = 0; y < w; ++y)
+				m[x].push(' ')
+		}
+		for(let robot of robots) {
+			let {x, y, vx, vy} = robot;
+			// console.log(x, y);
+			x += vx;
+			y += vy;
+			if(x < 0)
+				x += w;
+			if(y < 0)
+				y += h;
+			if(x >= w)
+				x -= w;
+			if(y >= h)
+				y -= h;
+			robot.x = x;
+			robot.y = y;
+			m[y][x] = '#';
+		}
+		// found this manually for aoc (saved 10k into a file and ctrl+f'd)
+		for(let l of m)
+			if(l.join('').includes('###############################'))
+				return i + 1;
+	}
 }
