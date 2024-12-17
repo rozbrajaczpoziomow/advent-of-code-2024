8c3cd9a476ac912ae14660790c0bd49b4ae987f1
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-17 19:35:57 +0100

    day 10 - tfw part 2 was accidentally solved in part 1

--- 10/day.js
+++ 10/day.js
@@ -6,21 +6,47 @@ const count = (arr, what) => arr.filter(el => el === what).length;
 const { abs, sqrt, sin, cos, floor, ceil, round, min, max } = Math;
 
 export async function part1() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
+	let map = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n').map(x=>x.split('').map(y=>+y));
+	let n = 0;
+	for(let y = 0; y < map.length; ++y) {
+		for(let x = 0; x < map[y].length; ++x) {
+			if(map[y][x] != 0)
+				continue;
 
-	for(const line of input) {
-
+			let pos = [[x,y,1]];
+			let reached = new Set;
+			while(pos.length) {
+				const [tx,ty,next] = pos.pop();
+				if(map[ty] != null && map[ty][tx+1] != null && map[ty][tx+1] == next) {if(next==9){reached.add((ty) * 100000 + (tx+1));}else{pos.push([tx+1,ty,next+1])}}
+				if(map[ty] != null && map[ty][tx-1] != null && map[ty][tx-1] == next) {if(next==9){reached.add((ty) * 100000 + (tx-1));}else{pos.push([tx-1,ty,next+1])}}
+				if(map[ty+1] != null && map[ty+1][tx] != null && map[ty+1][tx] == next) {if(next==9){reached.add((ty+1) * 100000 + (tx));}else{pos.push([tx,ty+1,next+1])}}
+				if(map[ty-1] != null && map[ty-1][tx] != null && map[ty-1][tx] == next) {if(next==9){reached.add((ty-1) * 100000 + (tx));}else{pos.push([tx,ty-1,next+1])}}
+			}
+			n+=reached.size;
+		}
 	}
-	return null;
+
+	return n;
 }
 
 export async function part2() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
+	let map = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n').map(x=>x.split('').map(y=>+y));
+	let n = 0;
+	for(let y = 0; y < map.length; ++y) {
+		for(let x = 0; x < map[y].length; ++x) {
+			if(map[y][x] != 0)
+				continue;
 
-	for(const line of input) {
-
+			let pos = [[x,y,1]];
+			while(pos.length) {
+				const [tx,ty,next] = pos.pop();
+				if(map[ty] != null && map[ty][tx+1] != null && map[ty][tx+1] == next) {if(next==9){++n;}else{pos.push([tx+1,ty,next+1])}}
+				if(map[ty] != null && map[ty][tx-1] != null && map[ty][tx-1] == next) {if(next==9){++n;}else{pos.push([tx-1,ty,next+1])}}
+				if(map[ty+1] != null && map[ty+1][tx] != null && map[ty+1][tx] == next) {if(next==9){++n;}else{pos.push([tx,ty+1,next+1])}}
+				if(map[ty-1] != null && map[ty-1][tx] != null && map[ty-1][tx] == next) {if(next==9){++n;}else{pos.push([tx,ty-1,next+1])}}
+			}
+		}
 	}
-	return null;
+
+	return n;
 }
