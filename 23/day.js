1f99545c09322a0644ea2a62fd8cc76fb616aab9
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-23 07:12:56 +0100

    day 23 - oh the nx

--- 23/day.js
+++ 23/day.js
@@ -10,18 +10,63 @@ export async function part1() {
 	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
+	let nw = {};
 
+	for(const line of input) {
+		let [fr,to] = line.split('-');
+		nw[fr] ??= [];
+		nw[fr].push(to);
+		nw[to] ??= [];
+		nw[to].push(fr);
 	}
-	return null;
+
+	let keys = Object.keys(nw);
+
+	for(let i = 0; i < keys.length; ++i)
+		for(let j = i+1; j < keys.length; ++j)
+			for(let k = j+1; k < keys.length; ++k) {
+				let a = keys[i], b = keys[j], c = keys[k];
+				// console.log(a,b,c);
+				if(nw[b].includes(a) && nw[c].includes(a) && nw[c].includes(b))
+					if(a[0] == 't' || b[0] == 't' || c[0] == 't')
+						++n;
+			}
+	return n;
 }
 
 export async function part2() {
 	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
+	let nw = {};
 
+	for(const line of input) {
+		let [fr,to] = line.split('-');
+		nw[fr] ??= [];
+		nw[fr].push(to);
+		nw[to] ??= [];
+		nw[to].push(fr);
 	}
-	return null;
+
+	let keys = Object.keys(nw);
+
+	let best = [];
+	for(let i = 0; i < 1500; ++i) {
+		keys.sort(() => Math.random() < .5? 1 : -1);
+		let curr = [];
+		for(let comp of keys) {
+			let conn = true;
+			for(let ccomp of curr) {
+				if(!nw[ccomp].includes(comp)) {
+					conn = false;
+					break;
+				}
+			}
+			if(conn)
+				curr.push(comp);
+		}
+		if(curr.length > best.length)
+			best = curr;
+	}
+	return best.toSorted().join(',');
 }
