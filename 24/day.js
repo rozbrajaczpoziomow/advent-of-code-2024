30a45326114ff118ddcc7bcfd7e0e428788ab0b3
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-26 03:29:28 +0100

    day 24 - circuits and full-adders my beloved!

--- 24/day.js
+++ 24/day.js
@@ -7,21 +7,31 @@ const arreq = (a, b) => a.length == b.length && a.every((v, i) => v == b[i]);
 const { abs, sqrt, sin, cos, floor, ceil, round, min, max, trunc } = Math;
 
 export async function part1() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
-
-	for(const line of input) {
-
+	let [init, gates] = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n\n');
+	const states = {};
+	for(let l of init.split('\n')) {
+		let [n,v] = l.split(': ');
+		states[n] = v == '1';
 	}
-	return null;
+	let todo = []; // {a,b,res,op}[]
+	for(let l of gates.split('\n')) {
+		let [a, op, b, _, res] = l.split(' ');
+		todo.push({a,b,res,op});
+	}
+	while(todo.length) {
+		let {a,b,res,op} = todo.shift();
+		if(states[a] == null || states[b] == null) {
+			todo.push({a,b,res,op});
+			continue;
+		}
+		// console.log(a,op,b,res,)
+		states[res] = (op == 'AND'? (states[a] && states[b]) : op == 'OR'? (states[a] || states[b]) : (states[a] != states[b]));
+	}
+	// console.log(states);
+	return parseInt(Object.keys(states).filter(x=>x[0]=='z').toSorted().toReversed().map(x=>states[x]?'1':'0').join(''),2);
 }
 
 export async function part2() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
-	let ns = [], n = 0;
-
-	for(const line of input) {
-
-	}
-	return null;
+	// solved manually
+	return 'jgb,rkf,rrs,rvc,vcg,z09,z20,z24';
 }
