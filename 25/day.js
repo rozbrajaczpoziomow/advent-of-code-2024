1e5f2378de095f0b3196e30cd584b48f9373aed1
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2024-12-26 03:26:23 +0100

    day 25 - :3

--- 25/day.js
+++ 25/day.js
@@ -7,13 +7,34 @@ const arreq = (a, b) => a.length == b.length && a.every((v, i) => v == b[i]);
 const { abs, sqrt, sin, cos, floor, ceil, round, min, max, trunc } = Math;
 
 export async function part1() {
-	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n');
+	let input = (await readFile(process.env.IN ?? 'input.txt')).toString().split('\n\n');
 	let ns = [], n = 0;
 
-	for(const line of input) {
+	let keys=[],locks=[];
 
+	const height = 6;
+
+	for(const line of input) {
+		let [A, ...lines] = line.split('\n');
+		let key = A[0] == '.';
+		let heights = new Array(A.length).fill(key? -1 : 0);
+		for(let i = 0; i < heights.length; ++i) {
+			for(let l of lines)
+				heights[i] += l[i] == '#';
+		}
+		if(key)keys.push(heights);else locks.push(heights);
 	}
-	return null;
+
+	for(let key of keys) {
+		for(let lock of locks) {
+			let yes = true;
+			for(let i = 0; i < key.length; ++i)
+				yes &= key[i] + lock[i] < height;
+			n+=yes;
+		}
+	}
+
+	return n;
 }
 
 export async function part2() {
@@ -23,5 +44,5 @@ export async function part2() {
 	for(const line of input) {
 
 	}
-	return null;
+	return 'hi';
 }
